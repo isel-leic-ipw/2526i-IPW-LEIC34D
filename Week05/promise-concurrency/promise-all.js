7d45096027b8c94d76a41978b2f3dac6cab364d5
Fernanda Passos <fernanda@midiacom.uff.br>
2025-10-08 13:58:59 +0100

    Promise concurrency examples.

---
+++ Week05/promise-concurrency/promise-all.js
@@ -0,0 +1,50 @@
+const urlArray = [
+    "https://eloquentjavascript.net/05_higher_order.html",
+    "https://eloquentjavascript.net/11_async.html",
+    "https://eloquentjavascript.net/11_async.html"
+    //, "http://not.exist"
+];
+
+promiseArray = urlArray.map(url => fetch(url));
+
+Promise.all(promiseArray)                       // Promise<Array<Response>>
+    .then(arrResp => {
+        const arrPromiseText = [];
+        for (let resp of arrResp)
+            arrPromiseText.push(resp.text());
+        return Promise.all(arrPromiseText);
+    })                                          // Promise<Array<String>>
+    .then(arrText => {console.log(arrText.map(text => text.length)); return arrText}) // print the size of each text response.
+    .then(arrText => arrText.reduce((t1, t2) => t1 + t2.length, 0)) // Promise<Number>
+    .then(totalLen => console.log(totalLen))    // Promise<undefined>
+    .catch(err => console.error("ERROR!"));
+
+// Promise.all(promiseArray)    // Promise<Array<Promise<Response>>>
+//     .then(arrResp => Promise.all(arrResp.map(resp => resp.text())))  // Promise<Array<String>>
+//     .then(arrText => arrText.reduce((t1, t2) => t1 + t2.length, 0))  // Promise<Number>
+//     .then(totalLen => console.log(totalLen))                         // Promise<undefined>
+//     .catch(e => console.error("ERROR!!!", e));
+
+
+// An implementation of Promise.all:
+function promiseAll(arrayPromises){
+    const arrayValues = [];
+    let count = 0;
+    let rejected = false;
+    return new Promise((resolve, reject) => {
+        for (let p of arrayPromises){
+            p.then(value => {
+                arrayValues.push(value);
+                count++;
+                if (count == arrayPromises.length)
+                    resolve(arrayValues);
+            }).catch(err => {
+                if (! rejected){
+                    reject(err);
+                    rejected = true;
+                }
+            });
+
+        }
+    });
+}
